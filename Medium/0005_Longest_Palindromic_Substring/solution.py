56f7e77bc067212e140aacd4a3dc44c9fc252593
manasauriz <izzymansoor42@gmail.com>
2025-03-16 11:33:10 +0530

    Restructuring Files

---
+++ Medium/0005_Longest_Palindromic_Substring/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        left, right = 0, 2
+        current_left, current_right = 1, 1
+        current = s[current_left, current_left]
+        
+        # check if there is a palindrome in current substring
+        
+        return
+    
+if __name__ == "__main__":
+    factory = Solution()
+    print(factory.longestPalindrome("papad"))
