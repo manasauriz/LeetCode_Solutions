472c1470a2f5681e2a5b150cc1e6a7cce6a731d6
manasauriz <izzymansoor42@gmail.com>
2025-03-16 12:30:50 +0530

    5. Longest Palindromic Substring in Python

--- Medium/0005_Longest_Palindromic_Substring/solution.py
+++ Medium/0005_Longest_Palindromic_Substring/solution.py
@@ -1,13 +1,29 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        left, right = 0, 2
-        current_left, current_right = 1, 1
-        current = s[current_left, current_left]
-        
-        # check if there is a palindrome in current substring
-        
-        return
-    
-if __name__ == "__main__":
-    factory = Solution()
-    print(factory.longestPalindrome("papad"))
+        pal = s[:1]
+        for i in range(len(s)):
+            loop_times = 1
+            if i < len(s) - 1 and s[i] == s[i + 1]:
+                loop_times += 1
+            
+            for j in range(loop_times):
+                if j == 0:
+                    left, right = i - 1, i + 1
+                    cur = s[i:right]
+                else:
+                    left, right = i - 1, i + 2
+                    cur = s[i:right]
+
+                # check if there is a palindrome in current substring
+                while True:
+                    if left < 0 or right >= len(s):
+                        break
+                    if s[left] == s[right]:
+                        cur = s[left:right + 1]
+                        left -= 1
+                        right += 1
+                    else:
+                        break
+
+                pal = cur if len(cur) > len(pal) else pal
+        return pal
