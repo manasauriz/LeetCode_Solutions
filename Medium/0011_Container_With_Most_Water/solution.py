6c505dff23be71d9c9b3db2cfb59671ab50cec4c
manasauriz <izzymansoor42@gmail.com>
2025-03-23 16:22:03 +0530

    11. Container With Most Water in Python

---
+++ Medium/0011_Container_With_Most_Water/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height) -> int:
+        l, r = 0, len(height) - 1 # left and right pointers
+        max = 0
+
+        while r > l:
+            right, left = height[r], height[l]
+            if right > left:
+                cur = left * (r - l)
+                l += 1
+            else:
+                cur = right * (r - l)
+                r -= 1
+            max = cur if cur > max else max
+
+        return max
