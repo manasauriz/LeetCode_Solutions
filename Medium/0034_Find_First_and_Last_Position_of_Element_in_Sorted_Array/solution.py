d4fd654bb49a57391c776365bf92a6a9e21f66d4
manasauriz <izzymansoor42@gmail.com>
2025-04-16 14:12:44 +0530

    Alternate Solution to Problem 34

--- Medium/0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/solution.py
+++ Medium/0034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/solution.py
@@ -17,3 +17,40 @@ class Solution:
                     res[1] = rh
                 break
         return res
+    
+    # Using binary search after finding target
+    
+    # def searchRange(self, nums: list[int], target: int) -> list[int]:
+    #     lh, rh = -1, -1
+    #     l, r = 0, len(nums) - 1
+
+    #     while l <= r:
+    #         half = r - l // 2
+    #         if nums[half] < target:
+    #             l = half + 1
+    #         elif nums[half] > target:
+    #             r = half - 1
+    #         else:
+    #             lh, rh, = half, half
+    #             nl, nr = half + 1, half - 1
+
+    #             while l <= nr:
+    #                 nh = nr - l // 2
+    #                 if nums[nh] < target:
+    #                     l = nh + 1
+    #                     continue
+    #                 if nums[nh] == target:
+    #                     lh = nh
+    #                 nr = nh - 1
+              
+    #             while nl <= r:
+    #                 nh = r - nl // 2
+    #                 if nums[nh] < target:
+    #                     nl = nh + 1
+    #                     continue
+    #                 if nums[nh] == target:
+    #                     rh = nh
+    #                 r = nh - 1
+    #             break
+            
+    #     return [lh, rh]
