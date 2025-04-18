020e150aed2a8333b61f88fe08e03b1e4fd077d0
manasauriz <izzymansoor42@gmail.com>
2025-04-18 13:19:10 +0530

    Alternate solution to Problem 36

--- Medium/0036_Valid_Sudoku/solution.py
+++ Medium/0036_Valid_Sudoku/solution.py
@@ -28,3 +28,27 @@ class Solution:
                 h_map[c] = True
 
         return True
+    
+    # Alternate Solution, removing redundant checking
+    # Uses 'k' to keep track of box index
+    # 'k' is calculated in two parts: 
+    # 1. (i // 3) * 3 -> corresponds to 0, 3, 6
+    # 2. {j // 3} -> corresponds to 0, 1, 2
+    # Added together they cover all boxes -> 0 (and 1, 2), 3 (and 4, 5), 6 (and 7, 8) 
+
+    # def isValidSudoku(self, board: List[List[str]]) -> bool:
+    #     rows = [set() for _ in range(9)]
+    #     cols = [set() for _ in range(9)]
+    #     boxs = [set() for _ in range(9)]
+
+    #     for i in range(9):
+    #         for j in range(9):
+    #             cell = board[i][j]
+    #             if cell == ".": continue
+    #             k = ((i // 3) * 3) + (j // 3)
+    #             if cell in rows[i] or cell in cols[j] or cell in boxs[k]: return False
+    #             rows[i].add(cell)
+    #             cols[j].add(cell)
+    #             boxs[k].add(cell)
+                
+    #     return True
